fc8c86fcae00b8f2db458e6d562c7f109697b6f5
oladetoungee <demilad1998@gmail.com>
2025-04-07 19:30:07 +0100

    Enhance metadata for SEO and social sharing
    - Updated the metadata description for clarity and consistency.
    - Added Open Graph and Twitter card data to improve social media sharing with relevant images and descriptions.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,31 @@ const italiana = Italiana({
 
 export const metadata: Metadata = {
   title: 'Walter Photography | Documentary & Portrait Photography',
-  description: 'Walter Jeremiah is a visionary photographer capturing life\'s most precious moments through documentary and portrait photography. Based in Canada.',
+  description: "Walter Jeremiah is a visionary photographer capturing life's most precious moments through documentary and portrait photography. Based in Canada.",
   keywords: 'photography, documentary photography, portrait photography, wedding photography, modeling photography, Canada photographer',
+  openGraph: {
+    title: 'Walter Photography | Documentary & Portrait Photography',
+    description: "Walter Jeremiah is a visionary photographer capturing life's most precious moments through documentary and portrait photography. Based in Canada.",
+    url: 'https://walter-photography.vercel.app/',
+    siteName: 'Walter Photography',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/dn2oenkss/image/upload/v1744050515/WhatsApp_Image_2025-03-25_at_21.50.02_eomm6c.jpg',  
+        width: 1200,
+        height: 630,
+        alt: 'Walter Photography',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Walter Photography | Documentary & Portrait Photography',
+    description: "Walter Jeremiah is a visionary photographer capturing life's most precious moments through documentary and portrait photography. Based in Canada.",
+    images: ['https://res.cloudinary.com/dn2oenkss/image/upload/v1744050515/WhatsApp_Image_2025-03-25_at_21.50.02_eomm6c.jpg'], 
+  },
 };
+
 
 export default function RootLayout({
   children,
